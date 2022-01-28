865229ca36f5146c280e4cea1f6e425fb5f901f7
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-01-28 12:03:56 +0300

    add stopTimer

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
-"use strcit";
-const minutes = document.getElementById("minutes"),
-  seconds = document.getElementById("seconds");
-let timeMinute = minutes.textContent * 60;
-setInterval(() => {
-  const second = timeMinute % 60,
-    minute = (timeMinute / 60) % 60;
-  seconds.innerHTML = Math.floor(second);
-  minutes.innerHTML = Math.floor(minute);
-  timeMinute--;
-}, 1000);
+"use strict";
+const pomidoroTimer = () => {
+  const minutes = document.getElementById("minutes"),
+    seconds = document.getElementById("seconds"),
+    time = document.getElementById("time");
+  let timeMinute = minutes.textContent * 60;
+  const zeroFormat = (number) => {
+    if (number < 10) {
+      number = "0" + number;
+    }
+    return number;
+  };
+  const startTimer = () => {
+    const second = timeMinute % 60,
+      minute = (timeMinute / 60) % 60;
+    if (timeMinute >= 0) {
+      seconds.textContent = zeroFormat(Math.floor(second));
+      minutes.textContent = zeroFormat(Math.floor(minute));
+      timeMinute--;
+    } else {
+      stopTimer();
+      defoultStyle();
+    }
+  };
+  let idInterval = setInterval(startTimer, 1000);
+  const stopTimer = () => {
+    clearInterval(idInterval);
+  };
+  const defoultStyle = () => {
+    seconds.style.color = "red";
+    minutes.style.color = "red";
+  };
+  const getEdit = () => {
+    time.addEventListener("click", (event) => {
+      const target = event.target;
+      minutes.contentEditable = true;
+      stopTimer();
+    });
+  };
+  getEdit();
+};
+pomidoroTimer();
